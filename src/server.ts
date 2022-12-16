be30ccc114fe87ae961f505d2204899fa5f99ad9
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-12-16 16:00:07 +0000

    added a cat reply picture

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Telegraf } from 'telegraf'
 import getBotTokenOrQuit from './util/getBotToken';
 
@@ -9,6 +10,13 @@ bot.start((ctx) => ctx.reply("Hello!  Let's talk!"))
 bot.help((ctx) => ctx.reply('Hmm i am not programmed to be helpful, yet!'))
 bot.hears('hello', (ctx) => ctx.reply('Ok, I heard you say hello'))
 bot.command('sing', (ctx) => ctx.reply('La la la!  I got your command.'))
+bot.command('secret', async (ctx) => {
+    const name = ctx.message.from.first_name
+    const number = Math.floor(Math.random()*100000)
+    ctx.reply(`Wow ${name}! you unlocked my secret command!😳 here's a cute cat picture as your prize😎`)
+    ctx.replyWithPhoto(`https://cataas.com/cat?x=${number}`)
+}
+)
 
 bot.launch()
 
